729c5850dee12d60d271e804644deb0517ac2a2e
QiuYi <178673693@qq.com>
2024-12-18 17:52:09 +0800

    添加问题

--- prompt.py
+++ prompt.py
@@ -4,7 +4,7 @@ INSTRUCTIONS = """
 {{question}}
 ---
 请根据KEY_INFORMATION回答我的问题。KEY_INFORMATION可能有用，也可能没用，你需要从我给出KEY_INFORMATION中选出与我的问题最相关的那些，来为你的回答提供依据。回答一定要忠于原文，简洁但不丢信息，不要胡乱编造，也不需要解释。我的问题是什么语种，你就用什么语种回复。
-你的回复："""
+为了让回复更清晰美观，用markdown格式回复。你的回复："""
 
 PROMPT_TEMPLATE = f"""
 <INSTRUCTIONS>
