5e993e33f23771945f639b43744c26d29e57d683
QiuYi <178673693@qq.com>
2025-01-13 11:54:14 +0800

    add 召回脚本cmd test

---
+++ script/04_recall_test_bm25_vec_rerank_cmd.py
@@ -0,0 +1,40 @@
+import sys
+
+import requests
+import json
+
+def send_request(question):
+    url = 'http://127.0.0.1:8777/api/local_doc_qa/local_doc_chat'
+    headers = {
+        'content-type': 'application/json'
+    }
+    data = {
+        "user_id": "zzp",
+        "kb_ids": ["KBf42f9bab99fa4ebd93a59be1e42f1969"],
+        "history": [],
+        "question": question,
+        "rerank": True,
+    }
+    try:
+        response = requests.post(url=url, headers=headers, json=data, timeout=60)
+        res = response.json()
+        doc_list = []
+        for doc in res['source_documents']:
+            content = doc['content']
+            doc_list.append(content)
+
+        return doc_list
+    except Exception as e:
+        print(f"请求发送失败: {e}")
+
+if __name__ == '__main__':
+
+    if len(sys.argv) < 2:
+        print("请输入问题。 Usage: python 04_recall_test_bm25_vec_rerank_cmd.py '问题'")
+        exit(1)
+
+    question = sys.argv[1]
+    docs = send_request(question)
+    print(f"问题: {question}")
+    print(f"文档: {docs}")
+    print("=" * 50)
